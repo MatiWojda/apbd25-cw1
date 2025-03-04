9dda137ec0ab2cfdebd1cf8a4141398b32f0b17b
Mateusz Wojda <s31096@pjwstk.edu.pl>
2025-03-04 16:45:29 +0100

    Initial project

--- Ćwiczenia1/Ćwiczenia1/Program.cs
+++ Ćwiczenia1/Ćwiczenia1/Program.cs
@@ -2,4 +2,12 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("HI");
 Console.WriteLine("HI");
+Console.WriteLine("HI");
 
+static double GetAverage(int[] arr) 
+{
+    return 0;
+}
+
+int[] arr = {1,2,3,4,5};
+GetAverage(arr);
